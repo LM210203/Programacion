360e79425cd688eaa44d77b45230d462251557a6
Luis Mendez <lamendeza@alumno.uspg.edu.gt>
2026-02-09 18:38:31 -0600

    Codigo con Bug

---
+++ Debuggin_test.py
@@ -0,0 +1,12 @@
+password = input("Clave: ")
+
+print(f"DEBUG tipo: {type(password)}")
+
+print(f"DEBUG valor: [{password}]")
+
+print(f"DEBUG len: {len(password)}")
+
+if password == 1234: # Bug aquí!
+ print("Acceso concedido")
+else:
+ print("Acceso denegado")
