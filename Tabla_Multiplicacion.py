b72504eb3b25029de780c25770ace238dc9526f1
Luis Mendez <lamendeza@alumno.uspg.edu.gt>
2026-03-07 15:41:12 -0600

    Del 1 al 12

--- Tabla_Multiplicacion.py
+++ Tabla_Multiplicacion.py
@@ -1,5 +1,6 @@
-num = int(input("Tabla del? "))
-print (f"\n--- Tabla del {num} ---")
-for i in range(1, 13):
+
+print (f"\n--- Tabla del 1 al 12---")
+for num in range(1, 13):
+ for i in range(1, 13):
     resultado = num * i
     print (f"{num} x {i:2d} = {resultado:3d}")
